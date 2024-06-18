99ab8b503d535f62106bd61a6c8c7aa26088d3c9
RAM <48535575+rmasinelli@users.noreply.github.com>
2024-06-18 14:53:35 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# main.py
 
+import tkinter as tk
+from ui import EduGame
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = EduGame(root)
+    root.geometry("400x400")  # Set the window size
+    root.mainloop()
